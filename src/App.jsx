876627f3c9895fda4bbe4740aa038aa5879113c8
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-04-18 18:26:58 -0300

    criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { NhostProvider } from "@nhost/react";
+import { nhost } from "./lib/nhost.js";
+import SignIn from "./signin.jsx";
+import Todos from "./todos.jsx";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(nhost.auth.getSession());
+
+    nhost.auth.onAuthStateChanged((_, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <NhostProvider nhost={nhost}>
+      {session ? <Todos session={session} /> : <SignIn />}
+    </NhostProvider>
+  );
+}
+
+export default App;
