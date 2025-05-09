447601a62a569776c26c9556ac4024cc8af7c659
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-05-09 19:54:47 -0300

    fix:Botão escalável
    Tornando o botão mais adaptável para diversos tipos de tamanho.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-export default function Button(props) {
+export default function Button({ text, type, onClick }) {
   return (
     <button
-      className="py-2 bg-blue-500 rounded-sm w-11/12 cursor-pointer hover:bg-blue-700 hover:scale-101 duration-500 delay-400 mt-2"
-      onClick={props.onClick}
-      type={props.type}
+      className="py-2 bg-blue-500 w-full rounded-sm cursor-pointer hover:bg-blue-700 hover:scale-101 duration-500 delay-400 mt-4"
+      onClick={onClick}
+      type={type}
     >
       <p className="text-white font-bold tracking-wide text-base select-none">
-        {props.texto}
+        {text ? text : "Cadastrar"}
       </p>
     </button>
   );
