55ca1e242a1174538c7ed732d05c5a52ebfc6b88
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-05-15 19:56:30 -0300

    fix: Add backButton option

--- src/components/form-register.jsx
+++ src/components/form-register.jsx
@@ -1,3 +1,5 @@
+import BackButton from "./back-button";
+
 export default function FormRegister({
   children,
   title,
@@ -5,12 +7,15 @@ export default function FormRegister({
   titlePostion,
   titleSize,
   onSubmit,
+  backRoute,
 }) {
   return (
     <form
       onSubmit={onSubmit}
       className={`${weight} bg-white m-auto mt-6 px-5 py-8 rounded-lg`}
     >
+      {backRoute ? <BackButton to={backRoute} /> : <></>}
+
       <div className={`flex w-full justify-${titlePostion}`}>
         <h6 className={`text-blue-500 font-bold mb-4 text-${titleSize}`}>
           {title}
