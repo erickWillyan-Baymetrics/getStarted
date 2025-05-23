84253a7e1e6784abc3e104f4fa3db0fc687447be
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-05-23 19:13:16 -0300

    Fix: Mudando props do backbutton

--- src/components/back-button.jsx
+++ src/components/back-button.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 
-export default function BackButton(props) {
+export default function BackButton({ to }) {
   const navigate = useNavigate();
+  console.log("aaaa");
   return (
     <button
       className="flex items-center font-bold text-blue-500 hover:text-blue-700 cursor-pointer delay-200 duration-200"
-      onClick={navigate(props.to)}
+      onClick={() => navigate(to)}
     >
       <IoIosArrowBack size={20} /> Voltar
     </button>
